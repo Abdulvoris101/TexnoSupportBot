a9e98b5e862cbeffe92cf72a701bd4c013d8cca9
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-01 00:06:21 +0500

    Added static commands and sending to adminstrator

--- apps/main/handlers.py
+++ apps/main/handlers.py
@@ -1,6 +1,16 @@
-from bot import dp
+from bot import dp, bot
 from aiogram import types
+from . import text
+import os
+
+USER_ID = os.environ.get("USER_ID") # set user_id to send messages to him.
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    await message.answer("HI")
+    await message.answer(text.START)
+
+@dp.message_handler()
+async def message_handler(message: types.Message):
+    await bot.send_message(USER_ID, message.text)
+    await message.answer(text.WAIT)
+
---
+++ apps/main/text.py
@@ -0,0 +1,5 @@
+START="""Assalomu aleykum, Men yordamchi botman
+Savol yoki taklifingiz bo'lsa shu yerga yozing. 
+Adminstratorlar tez orada javob beradi 😊"""
+
+WAIT = """Savol yoki taklifingizga tez orada adminstratorlar javob berishadi. Iltimos biroz kuting..."""
